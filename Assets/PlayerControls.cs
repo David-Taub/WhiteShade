cbd3f5889f93bb93851cbc6c097a58248e0329e0
David Ariel <davidariel333@gmail.com>
2018-02-05 16:04:14 +0100

    better walk animation and controls

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -6,24 +6,47 @@ using System;
 public class PlayerControls : MonoBehaviour {
 
     private float speed = 5.0f;
-    private float step = 2.0f;
+    private float step = 1.0f;
     private Vector3 target;
-
+    Animator amin;
  
     void Start()
     {
         target = transform.position;
+        amin = GetComponent<Animator>();
     }
     void Update()
     {
 
         if (transform.position == target)
         {
-            float x = Math.Sign(Input.GetAxis("Horizontal"));
-            float y = Math.Sign(Input.GetAxis("Vertical"));
+            float x = 0, y = 0;
+            x = Math.Sign(Input.GetAxis("Horizontal"));
+            y = Math.Sign(Input.GetAxis("Vertical"));
             
             target += new Vector3(x, y, 0);
+            if (transform.position == target)
+                amin.SetInteger("State", 0);
+           
         }
+        if (transform.position.x < target.x)
+        {
+            amin.SetInteger("State", 1);
+            Vector3 localScale = transform.localScale;
+            localScale.x = -1;
+            transform.localScale = localScale;
+        }
+        if (transform.position.x > target.x)
+        {
+            amin.SetInteger("State", 4);
+            Vector3 localScale = transform.localScale;
+            localScale.x = 1;
+            transform.localScale = localScale;
+        }
+        if (transform.position.y < target.y)
+            amin.SetInteger("State", 2);
+        if (transform.position.y > target.y)
+            amin.SetInteger("State", 3);
         transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
 
     }
