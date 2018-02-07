e39316877a40295dd7677fc848c393847412e0a4
David Ariel <davidariel333@gmail.com>
2018-02-07 14:27:07 +0100

    a* Pathfinding project uploaded right click to move not working properly yet

--- Assets/Scripts/MapCreator.cs
+++ Assets/Scripts/MapCreator.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using Pathfinding;
+using UnityEngine;
 using System;
+
+using System.Threading;
 using System.Collections.Generic; 		//Allows us to use Lists.
 using Random = UnityEngine.Random; 		//Tells Random to use the Unity Engine random number generator.
 
@@ -9,43 +12,66 @@ public class MapCreator : MonoBehaviour {
     private Transform boardHolder;
     public int columns;
     public int rows;
+    private AutoResetEvent autoEvent;
+    public GridGraph graph;
     public float WALL_RATIO = 0.2f;
+    public float nodeDistance = 1.0f;
     private List<Vector3> freeLocations = new List<Vector3>();
     public void BoardSetup()
     {
-        for (int x = 0; x < columns; x++)
-        {
-            for (int y = 0; y < rows; y++)
+        AstarPath.active.AddWorkItem(new AstarWorkItem(ctx => {
+
+            for (int x = 0; x < columns; x++)
             {
-                GameObject toInstantiate = floorTile;
+                for (int y = 0; y < rows; y++)
+                {
+                    GameObject toInstantiate = floorTile;
 
-                //Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
-                if (x == 0 || x == columns - 1 || y == 0 || y == rows - 1)
-                    toInstantiate = wallTile;
-                else if (Random.value < WALL_RATIO)
-                    toInstantiate = wallTile;
-                else
-                    freeLocations.Add(new Vector3(x, y, 0));
+                    //Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
+                    GraphNode node = graph.GetNode(x, y);
+                    node.position = new Int3(x, y, 0);
+                    if (isForbiddenPosition(x, y) || (Random.value < WALL_RATIO))
+                    {
+                        toInstantiate = wallTile;
+                        node.Walkable = false;
+                    }
+                    else
+                    {
+                        freeLocations.Add(new Vector3(x, y, 0));
+                        node.Walkable = true;
+                    }
 
-                //Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
-                GameObject instance =
-                    Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                    //Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+                    GameObject instance =
+                        Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
 
-                //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
-                instance.transform.SetParent(boardHolder);
+                    //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+                    instance.transform.SetParent(boardHolder);
+                }
             }
-        }
+            graph.GetNodes(node => graph.CalculateConnections((GridNodeBase)node));
+            ctx.QueueFloodFill();
+            AstarPath.active.Scan();
+        }));
     }
+
+    private Boolean isForbiddenPosition(int x, int y)
+    {
+        return (x == 0 || x == columns - 1 || y == 0 || y == rows - 1);
+    }
+
     // Use this for initialization
     void Start() {
         boardHolder = new GameObject("Board").transform;
+        GraphSetup();
         BoardSetup();
-        Vector3 pos = RandomPosition();
-        GameObject Player = GameObject.FindGameObjectWithTag("Player");
-        Instantiate(Player, new Vector3(pos.x, pos.y, 0f), Quaternion.identity);
+        Vector3 pos = PopRandomPosition();
+        //GameObject Player = GameObject.FindGameObjectWithTag("Player");
+        GameObject player = GameObject.Find("Player");
+        player.transform.position = new Vector3(pos.x, pos.y, 0f);
     }
 
-    Vector3 RandomPosition()
+    Vector3 PopRandomPosition()
     {
         int randomIndex = Random.Range(0, freeLocations.Count);
         Vector3 randomPosition = freeLocations[randomIndex];
@@ -56,4 +82,11 @@ public class MapCreator : MonoBehaviour {
     void Update () {
 		
 	}
+    void GraphSetup()
+    {
+        graph = AstarPath.active.data.AddGraph(typeof(GridGraph)) as GridGraph;
+        graph.SetDimensions(columns, rows, nodeDistance);
+        AstarPath.active.Scan();
+       
+    }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,54 +1,95 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
 
 public class PlayerControls : MonoBehaviour {
 
-    private float speed = 5.0f;
-    private float step = 1.0f;
-    private Vector3 target;
+    private float speed = 10.0f;
+    private Vector3 stepTarget;
+    private Vector3 destination;
+    Queue<GraphNode> currentPath = new Queue<GraphNode>();
     Animator amin;
  
     void Start()
     {
-        target = transform.position;
+        destination = transform.position;
+        stepTarget = transform.position;
         amin = GetComponent<Animator>();
     }
-    void Update()
+    void UpdateState()
     {
-
-        if (transform.position == target)
-        {
-            float x = 0, y = 0;
-            x = Math.Sign(Input.GetAxis("Horizontal"));
-            y = Math.Sign(Input.GetAxis("Vertical"));
-            
-            target += new Vector3(x, y, 0);
-            if (transform.position == target)
-                amin.SetInteger("State", 0);
-           
-        }
-        if (transform.position.x < target.x)
+        if (transform.position == stepTarget)
+            amin.SetInteger("State", 0);
+        if (transform.position.x < stepTarget.x)
         {
             amin.SetInteger("State", 1);
             Vector3 localScale = transform.localScale;
             localScale.x = -1;
             transform.localScale = localScale;
         }
-        if (transform.position.x > target.x)
+        if (transform.position.x > stepTarget.x)
         {
             amin.SetInteger("State", 4);
             Vector3 localScale = transform.localScale;
             localScale.x = 1;
             transform.localScale = localScale;
         }
-        if (transform.position.y < target.y)
+        if (transform.position.y < stepTarget.y)
             amin.SetInteger("State", 2);
-        if (transform.position.y > target.y)
+        if (transform.position.y > stepTarget.y)
             amin.SetInteger("State", 3);
-        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
-
+    }
+    
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            currentPath = new Queue<GraphNode>(p.path);
+            Debug.Log("path found");
+            Debug.Log(p.path[0].position);
+        }
+            
     }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            //right click
+            destination = RoundVector3(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            destination.z = 0;
+            AstarPath.StartPath(ABPath.Construct(transform.position, destination, OnPathComplete));
+            Debug.Log("click at");
+            Debug.Log(destination);
+            Debug.Log("current");
+            Debug.Log(transform.position);
+        }
+        stepOnPath();
+        UpdateState();
+    }
+    private void stepOnPath()
+    {
+        if (transform.position == stepTarget)
+        {
+            if (currentPath.Count > 0)
+            {
+                GraphNode nextNode = currentPath.Dequeue();
+                GridGraph graph = (GridGraph) AstarPath.active.data.graphs[0];
+                stepTarget = new Vector3(nextNode.position.x, nextNode.position.y, nextNode.position.z);
+                Debug.Log(stepTarget);
+                Debug.Log(currentPath.Count);
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, stepTarget, Time.deltaTime * speed);
+        }
+    }
+
+    private Vector3 RoundVector3(Vector3 vec)
+    {
+        return new Vector3(Mathf.Round(vec.x), Mathf.Round(vec.y), Mathf.Round(vec.z));
+    }
 }
